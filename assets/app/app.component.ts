a59853fa0db5d15ff1189ab74a536197c592a026
Rahul Jadhav <rahul.jadhav.fidel@gmail.com>
2017-12-15 15:30:38 +0530

    added bootstap

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>jhgjhgjhgj<h1>',
+    templateUrl: './app.component.html',
     providers: []
 })
 export class AppComponent {}
